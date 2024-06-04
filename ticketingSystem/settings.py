8810bc310b26ab4578c4085fa100f1d466907e4f
Laban Kibet <64686919+Laban254@users.noreply.github.com>
2024-06-04 23:47:32 +0300

    Update settings.py

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-u0x&^du(9wn8zb5kjgfy%)l5_a-z$b+u9)@pvv38-$_3yy33$=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
