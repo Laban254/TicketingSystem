a13ca6a6a3a8c511fbf66771c45ca68b5d79b360
laban254 <labanrotich6544@gmail.com>
2024-06-10 22:58:53 +0300

    enable use of static dir in development

--- ticketingSystem/urls.py
+++ ticketingSystem/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('dashboard', include('dashboard.urls')),
     path('tickets/', include('tickets.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
