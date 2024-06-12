94fbd5f6b9eaf42fd0cc31460ecf21b480ce6f04
laban254 <labanrotich6544@gmail.com>
2024-06-12 23:25:04 +0300

    added whitenoise

--- ticketingSystem/settings.py
+++ ticketingSystem/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'whitenoise.runserver_nostatic',
 
     'widget_tweaks',
     'django.contrib.humanize',
@@ -63,6 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -141,11 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where collectstatic will store files
-# STATIC_ROOT = "/var/www/studyHub.com/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')  # Additional static files directories, if any
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
